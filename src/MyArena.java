b75d599d9ce4511950cdb5a8ab24962851025e1f
Vison Lee <b99902079@ntu.edu.tw>
2013-05-15 20:03:52 +0800

    pet init

--- src/MyArena.java
+++ src/MyArena.java
@@ -1,14 +1,22 @@
 package ntu.csie.oop13spring;
 
+import java.util.ArrayList;
+
 
 public class MyArena extends POOArena{
-	
+	private ArrayList<POOPet> allpets = new ArrayList<POOPet>(0);
+    
+    public void addPet(POOPet p){
+        allpets.add(p);
+        System.out.println("hi");
+    }
 	@Override
 	public boolean fight(){
-		return false;
 		
+		//for(int i=0;i<this.)
+		System.out.println(this.getAllPets());
+		return false;
 	}
-	
 	@Override
     public void show(){
     	
--- src/MyPet1.java
+++ src/MyPet1.java
@@ -2,12 +2,22 @@ package ntu.csie.oop13spring;
 
 
 public class MyPet1 extends POOPet{
-
+    private int HP, MP, AGI;
+    private String name;
+    private int totalNum = 0;
 	public MyPet1(){
-		System.out.println("success to load  MyPet1");
+		totalNum++;
+		setName("Water"+totalNum);
+		setAGI(AGI);
+		setHP(HP);
+		setMP(MP);
+		//System.out.println("success to load  MyPet1");
 	}
 	@Override
 	protected POOAction act(POOArena arena) {
+		POOAction action = new POOAction();
+		POOSkill my = new POOTinyAttackSkill();
+		action.skill = my;
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -18,4 +28,5 @@ public class MyPet1 extends POOPet{
 		return null;
 	}
 
+	
 }
